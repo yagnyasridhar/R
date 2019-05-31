1e25832fb0dce08c9ac54bc9371c341892ee8ca1
yagnyasridhar <32097084+yagnyasridhar@users.noreply.github.com>
2019-05-31 15:30:27 +0530

    Create box.R

---
+++ Charts/box.R
@@ -0,0 +1,8 @@
+png(file = "boxplot.png")
+
+# Plot the chart.
+boxplot(mpg ~ cyl, data = mtcars, xlab = "Number of Cylinders",
+   ylab = "Miles Per Gallon", main = "Mileage Data")
+
+# Save the file.
+dev.off()
