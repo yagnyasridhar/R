9385f5f29ec6a0cf18d75078ed24ac2d8756ed81
yagnyasridhar <32097084+yagnyasridhar@users.noreply.github.com>
2019-05-31 15:29:33 +0530

    Create scatter.R

---
+++ Charts/scatter.R
@@ -0,0 +1,18 @@
+input <- mtcars[,c('wt','mpg')]
+
+# Give the chart file a name.
+png(file = "scatterplot.png")
+
+# Plot the chart for cars with weight between 2.5 to 5 and mileage between 15 and 30.
+plot(x = input$wt,y = input$mpg,
+   xlab = "Weight",
+   ylab = "Milage",
+   xlim = c(2.5,5),
+   ylim = c(15,30),		 
+   main = "Weight vs Milage"
+)
+
+pairs(~wt+mpg+disp+cyl,data = mtcars,
+   main = "Scatterplot Matrix")
+
+	
